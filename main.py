46e938f8a5c656757379cfbfd8cb82a11507c4f8
Andrew <avanostra@gmail.com>
2023-11-12 05:01:20 -0500

    Finalv2

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 import collections
 import time
 import pygame
+import turtle as tur
 
 # Initialize Polly and SageMaker clients
 sagemaker_runtime = boto3.client('sagemaker-runtime', region_name='us-east-1')
@@ -17,6 +18,71 @@ class_names = ['A', 'B', 'C', 'None']
 predictions = collections.deque(maxlen=100)  # Adjust based on your webcam's frame rate
 start_time = time.time()
 should_speak = True  # Flag to control speech
+
+
+#define drawing functions
+import time
+t=tur.Turtle()
+t.speed(100)
+t.penup()
+t.goto(-30,-50) #centering in the screen
+t.pensize(10)
+t.pencolor("black")
+t.hideturtle()
+
+def letter_draw(lett):
+    t.clear()
+    t.goto(-30,50)
+    t.right(t.towards(0,50))
+    if lett == 'a':   
+        t.pendown()
+        t.right(65)
+        t.forward(100)
+
+        t.setpos(-30,50)
+        t.right(50)
+        t.forward(100)
+
+        t.penup()
+        t.setpos(-50,-10)
+        t.right(65)
+        t.pendown()
+        t.backward(50)
+        t.penup()
+        t.right(180)
+    
+    if lett == 'b':  
+        t.penup()
+#draw straight line
+        t.goto(-30,50) #centering in the screen
+        t.pendown()
+        t.goto(-30,-150)
+
+        t.penup()
+        t.goto(-30,50) #centering in the screen
+        #draw first curve
+        t.pendown()
+        #t.right(-90)
+        t.circle(-50,180)
+
+
+        t.penup()
+        t.goto(-30,-50) #centering in the screen
+        #draw second curve
+        t.pendown()
+        t.right(180)
+        t.circle(-50,180)
+        t.penup()
+        t.right(180)
+    if lett == 'c':
+        t.goto(-30,50) #centering in the screen
+        t.pendown()
+        t.pensize(10)
+        t.right(180)
+        t.circle(50,180)
+        t.penup()
+
+
 
 # Define the preprocess_frame function
 def preprocess_frame(frame):
@@ -68,7 +134,9 @@ while True:
         if predictions and should_speak:
             # Find the most common prediction
             most_common_prediction = collections.Counter(predictions).most_common(1)[0][0]
+            letter_draw(most_common_prediction.lower())
             print(f"Most common prediction in the last 5 seconds: {most_common_prediction}")
+
 
             # Call Amazon Polly to synthesize speech
             response = polly_client.synthesize_speech(
